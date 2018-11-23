5c6fc22acfb6ac1896cb592a85630e794ba24470
simon-an <nomisimon@posteo.de>
2018-11-23 18:40:43 +0100

    implement safes load actions and reducer

--- src/app/shared/store/safe/actions/safe-list.actions.ts
+++ src/app/shared/store/safe/actions/safe-list.actions.ts
@@ -1,11 +1,21 @@
 import { Action } from '@ngrx/store';
+import { Safe } from '~core/model';
 
 export enum SafeListActionTypes {
-  LoadSafeLists = '[SafeList] Load SafeLists'
+  LoadSafeLists = '[SafeList] Load SafeLists',
+  LoadSafeListsSuccess = '[SafeList] Load SafeLists Success',
+  LoadSafeListsFailure = '[SafeList] Load SafeLists Failure'
 }
 
 export class LoadSafeLists implements Action {
   readonly type = SafeListActionTypes.LoadSafeLists;
 }
+export class LoadSafeListsSuccess implements Action {
+  readonly type = SafeListActionTypes.LoadSafeListsSuccess;
+  constructor(public payload: { safes: Safe[] }) {}
+}
+export class LoadSafeListsFailure implements Action {
+  readonly type = SafeListActionTypes.LoadSafeListsFailure;
+}
 
-export type SafeListActions = LoadSafeLists;
+export type SafeListActions = LoadSafeLists | LoadSafeListsSuccess | LoadSafeListsFailure;
--- src/app/shared/store/safe/reducers/safe-list.reducer.ts
+++ src/app/shared/store/safe/reducers/safe-list.reducer.ts
@@ -1,21 +1,25 @@
 import { Action } from '@ngrx/store';
 import { SafeListActions, SafeListActionTypes } from '../actions/safe-list.actions';
+import { Safe } from '~core/model';
 
 export interface State {
-
+  safes: Safe[];
+  pending: boolean;
 }
 
 export const initialState: State = {
-
+  safes: [],
+  pending: false
 };
 
 export function reducer(state = initialState, action: SafeListActions): State {
   switch (action.type) {
-
     case SafeListActionTypes.LoadSafeLists:
-      return state;
-
-
+      return { ...state, pending: true };
+    case '[SafeList] Load SafeLists Success':
+      return { safes: [...action.payload.safes], pending: false };
+    case SafeListActionTypes.LoadSafeListsFailure:
+      return { ...state, pending: false };
     default:
       return state;
   }
